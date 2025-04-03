1d88d5f31b1a41c7ef343ab31ffda9147706a625
dev-abhishekkr <dev.abhishekkr@gmail.com>
2025-04-03 15:27:46 +0530

    minor

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,9 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { AuthOptions } from "next-auth";
 import { connectDB } from "@/lib/db";
 import { User } from "@/models/User";
 
-export const authOptions: AuthOptions = {
+const handler = NextAuth({
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
@@ -35,7 +34,6 @@ export const authOptions: AuthOptions = {
     pages: {
         signIn: "/",
     },
-};
+});
 
-const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
